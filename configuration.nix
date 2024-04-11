0e652f78a651049b0aa36e73ed267f6db04ba8c7
zlepper <hansen13579@gmail.com>
2024-04-11 14:52:14 +0200

    Ensure sharp font

--- configuration.nix
+++ configuration.nix
@@ -170,6 +170,33 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-emoji
+    ];
+
+    fontconfig = {
+      # Fixes pixelation
+      antialias = true;
+
+      # Fixes antialiasing blur
+      hinting = {
+        enable = true;
+        style = "full"; # no difference
+        autohint = true; # no difference
+      };
+
+      subpixel = {
+        # Makes it bolder
+        rgba = "rgb";
+        lcdfilter = "default"; # no difference
+      };
+    };
+  };
+
+
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
