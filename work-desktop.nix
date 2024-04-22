cfff3de446b0ef95f6c853e8c7a057a09505a331
zlepper <hansen13579@gmail.com>
2024-04-22 12:01:47 +0200

    Enable ghostscript on work machine

--- work-desktop.nix
+++ work-desktop.nix
@@ -1,9 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [ ./hardware/work-desktop.nix ./configuration.nix ];
 
   networking.hostName = "rhdh-work-desktop";
 
+  environment.systemPackages = with pkgs; [ ghostscript ];
   use-home-manager.enable = true;
 }
