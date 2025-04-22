5e9363a05d55ab432f08a4bcb48ae980067e8441
zlepper <hansen13579@gmail.com>
2025-04-22 10:26:39 +0200

    Add libpcap to rust-rover

--- software/development/rust.nix
+++ software/development/rust.nix
@@ -1,9 +1,26 @@
-{unstable, pkgs, ...}:
-{
+{unstable, pkgs, lib, ...}:
+
+let
+ runtimeDeps = lib.makeLibraryPath [
+    pkgs.libpcap
+ ];
+
+ rust-rover = pkgs.symlinkJoin {
+    name = "rust-rover";
+    paths = [ unstable.jetbrains.rust-rover ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+        wrapProgram $out/bin/rust-rover \
+          --suffix "LIBPCAP_LIBDIR" : "${runtimeDeps}" \
+    '';
+    };
+in {
     home.packages = [
+        rust-rover
         pkgs.rustup
         pkgs.clang
         pkgs.heaptrack
-        unstable.jetbrains.rust-rover
     ];
 }
+
+
