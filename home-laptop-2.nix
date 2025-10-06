1a695601ad5669eea6676d9206b01ce9066b6256
zlepper <hansen13579@gmail.com>
2025-10-06 08:12:42 +0200

    Update nix packages

--- home-laptop-2.nix
+++ home-laptop-2.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, unstable, ... }:
 
 {
   imports = [ ./hardware/home-laptop-2.nix ./configuration.nix ];
@@ -6,6 +6,11 @@
   networking.hostName = "home-laptop-2";
 
   use-home-manager.enable = true;
+
+  environment.systemPackages = [
+     unstable.claude-code
+     pkgs.ripgrep
+  ];
  
   system.stateVersion = "25.05";
 }
